b6b4431810c94628b63645b409c2d51a29ca0830
Puthtipong Thunyatada <puthtipong.t@gmail.com>
2023-11-02 10:38:05 +0700

    Added test code to show variables transferred

--- Member.php
+++ Member.php
@@ -9,5 +9,29 @@
     <?php
             echo "<h3>This is members only</h3>"
         ?>
+    <?php
+    session_start();
+
+    // Check if the user is logged in
+    if (isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+        $userType = $_SESSION['userType'];
+        $memberType = $_SESSION['memberType'];
+        $faculty = $_SESSION['faculty'];
+        $doe = $_SESSION['doe'];
+
+        echo "Welcome, $username!<br>";
+        echo "User Type: $userType<br>";
+
+        if ($userType == "member") {
+            echo "Member Type: $memberType<br>";
+            echo "Faculty: $faculty<br>";
+            echo "Date of Enrollment: $doe<br>";
+        }
+    } else {
+        // Handle the case when the user is not logged in or session data is not set
+        echo "You are not logged in.";
+    }
+    ?>
     </body>
 </html>
--- User.php
+++ User.php
@@ -10,5 +10,20 @@
             echo "<h3>This is general users page</h3>"
             // get the user full name to show here
         ?>
+        <?php
+        session_start();
+
+        // Check if the user is logged in
+        if (isset($_SESSION['username'])) {
+            $username = $_SESSION['username'];
+            $userType = $_SESSION['userType'];
+
+            echo "Welcome, $username!<br>";
+            echo "User Type: $userType<br>";
+        } else {
+            // Handle the case when the user is not logged in or session data is not set
+            echo "You are not logged in.";
+        }
+        ?>
     </body>
 </html>
