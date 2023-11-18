d0ca0ebde011e5caac6e3422795cb9c3de65bd0e
GreenyScrapy <greenscrap320@gmail.com>
2023-11-18 14:29:20 +0700

    Finished styling the Add book and edit book pages

--- AddBook.php
+++ AddBook.php
@@ -1,18 +1,62 @@
 <!-- <form action="" class="popup center column add-book" id="popup" method="post"> -->
-<div class="row">
-    <h1>Add a new book</h1>
-</div>
-<div class="row">
-    <div class="column">
-        <h2>Name</h2>
-        <input type="text" id="Name" name="Name" required>
-        <h2>Author Name</h2>
-        <input type="text" id="Author" name="Author" required>
-        <h2>Genre</h2>
-        <input type="text" id="Genre" name="Genre" required>
-        <h2>ISBN</h2>
-        <input type="text" id="ISBN" name="ISBN" required>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Admin</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;700&display=swap" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+</head>
+
+<body class="center">
+    <div class="headbar">
+        <img src="pictures\CSS326BasedLib.png" alt="Logo">
+        <div class="greet-text">
+            <p>Add a new book</p><br>
+        </div>
     </div>
-</div>
-<input type="submit" value="Confirm" style="color: green">
-<input type="button" value="Cancel" style="color: red" onclick="PopDown()">
+    <form class="manage-panel add-book">
+        <div class="row" style="width: 88%;">
+            <div class="column">
+                <h3>Name</h3>
+            </div>
+            <div class="column" style="width: 73%;"><input type="text" id="Name" name="Name" required></div>
+        </div>
+        <div class="row" style="width: 88%;">
+            <div class="column">
+                <h3>Author Name</h3>
+            </div>
+            <div class="column" style="width: 73%;">
+                <input type="text" id="Author" name="Author" required>
+            </div>
+        </div>
+        <div class="row" style="width: 88%;">
+            <div class="column">
+                <h3>Genre</h3>
+            </div>
+            <div class="column" style="width: 73%;"><input type="text" id="Genre" name="Genre" required>
+            </div>
+        </div>
+        <div class="row" style="width: 88%;">
+            <div class="column">
+                <h3>ISBN</h3>
+            </div>
+            <div class="column" style="width: 73%;"><input type="text" id="ISBN" name="ISBN" required>
+            </div>
+        </div>
+        <div class="row center" style="padding: 0 0 0 0; justify-content: space-evenly;">
+        <input type="submit" value="Confirm" id="submit-button">
+        <input type="button" value="Cancel" id="submit-button">
+
+        </div>
+        
+    </form>
+
+</body>
+
+</html>
---
+++ EditBook.php
@@ -0,0 +1,62 @@
+<!-- <form action="" class="popup center column add-book" id="popup" method="post"> -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Admin</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;700&display=swap" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+</head>
+
+<body class="center">
+    <div class="headbar">
+        <img src="pictures\CSS326BasedLib.png" alt="Logo">
+        <div class="greet-text">
+            <p>Edit book</p><br>
+        </div>
+    </div>
+    <form class="manage-panel add-book">
+        <div class="row" style="width: 88%;">
+            <div class="column">
+                <h3>Name</h3>
+            </div>
+            <div class="column" style="width: 73%;"><input type="text" id="Name" name="Name" required></div>
+        </div>
+        <div class="row" style="width: 88%;">
+            <div class="column">
+                <h3>Author Name</h3>
+            </div>
+            <div class="column" style="width: 73%;">
+                <input type="text" id="Author" name="Author" required>
+            </div>
+        </div>
+        <div class="row" style="width: 88%;">
+            <div class="column">
+                <h3>Genre</h3>
+            </div>
+            <div class="column" style="width: 73%;"><input type="text" id="Genre" name="Genre" required>
+            </div>
+        </div>
+        <div class="row" style="width: 88%;">
+            <div class="column">
+                <h3>ISBN</h3>
+            </div>
+            <div class="column" style="width: 73%;"><input type="text" id="ISBN" name="ISBN" required>
+            </div>
+        </div>
+        <div class="row center" style="padding: 0 0 0 0; justify-content: space-evenly;">
+        <input type="submit" value="Confirm" id="submit-button">
+        <input type="button" value="Cancel" id="submit-button">
+
+        </div>
+        
+    </form>
+
+</body>
+
+</html>
